2c4e753226d0d162b80708d971156d2066680469
Mihde9 <gabriel4190>
2020-06-22 01:44:54 +0100

    Initial Commit

---
+++ Calculator.py
@@ -0,0 +1,29 @@
+import re
+
+print("My Smart Calculator")
+print("Type 'end' to exit")
+
+previous = 0
+run = True
+
+def solveMath():
+    global run
+    global previous
+    equation = ""
+    if previous == 0:
+        equation = input("Enter your equation here:")
+    else:
+        equation = input(str(previous))
+
+    if equation == "end":
+        print("Bye bye")
+        run = False
+    else:
+        equation =re.sub('[a-zA-Z,.:()" "]', '',equation)
+        if previous == 0:
+            previous = eval(equation)
+        else:
+            previous = eval(str(previous)+equation)
+
+while run:
+    solveMath()
